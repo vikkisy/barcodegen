c63ea5fac29b8318b02fe37047bb1372f3299492
Vickie <vsysoup@gmail.com>
2017-11-09 15:05:24 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  colorsArr = [];
+  constructor(){
+      for (let i: number  = 0; i<= 10; i++) {
+          const rand = (Math.floor(Math.random() * 5)) + 1;
+          if (rand == 1) {
+              this.colorsArr.push('Green');
+            }
+          if (rand == 2) {
+            this.colorsArr.push('HotPink');
+          }
+          if (rand == 3) {
+            this.colorsArr.push('MediumTurquoise');
+          }
+          if (rand == 4) {
+            this.colorsArr.push('MediumVioletRed');
+          }
+          if (rand == 5) {
+            this.colorsArr.push('MidnightBlue');
+          }
+
+      }
+  }
+  
+  
+
+
 }
